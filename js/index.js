ece3b3259560b85b404c383cf393a343fe35eafd
matiasr1608 <matiasr1608@gmail.com>
2023-02-10 11:58:14 -0300

    fix destinations, markers,  version 0.0.4

--- js/index.js
+++ js/index.js
@@ -1,12 +1,13 @@
 // const { cssNumber } = require("jquery");
-
+const DENO_URL = 'donde-estan-buses.deno.dev'
 var lineas = { 10: ['2', '222', '402', '404', '405', '407', '409', '427', '456', '494', '4A', '4AC', '4AD', '4D', '4DR', '600', '76', 'CE1', 'D9', 'G', 'L14', 'L16', 'L29', 'L7'], 13: ['15A', '15B', '6A', '6R6'], 18: ['1A', '2A', '700', '701', '703', '704', '705', '706', '707', '708', '709', '710', '711', '712', '714', '724', '747', '748', '750', '751', '752', '757', '7A', '7E7R', '7E8R', '802', '803', '804', '809', '8A', '8E7R', '8E8R', 'DM1', 'P757', 'P758', 'P759', 'P760', 'P761', 'P768', 'P7H', 'XA1', 'XA2'], 20: ['1M1', '1M12', '1M6', '1M7', '2M1', '2M7', '2M8', '505', '522', '524', '526', '538', '546', '582', 'D11', 'L24', 'L25', 'L38', 'MD3', 'ML1'], 29: ['2K'], 32: ['S5', 'S6', 'S7'], 33: ['A1', 'A10', 'A11', 'A12', 'A14', 'A16', 'A18', 'A5', 'A6', 'A9'], 35: ['10A', '14A', '14AB', '14AR', 'T1', 'T14A', 'T2', 'T4N', 'T4R', 'T5', 'T6'], 36: [], 37: [], 39: ['Z1', 'Z2', 'Z3', 'Z4'], 50: ['100', '102', '103', '104', '105', '109', '110', '111', '112', '113', '115', '116', '117', '121', '124', '125', '127', '128', '130', '137', '141', '142', '143', '144', '145', '147', '148', '149', '150', '151', '155', '156', '157', '158', '163', '169', '174', '175', '180', '181', '183', '185', '186', '187', '188', '191', '192', '195', '199', '21', '214', '227', '230', '268', '276', '60', '62', '64', 'C1', 'C2', 'C3', 'C4', 'C5', 'CE1', 'D10', 'D5', 'D8', 'DE1', 'DM1', 'E14', 'G10', 'G11', 'G3', 'G8', 'L1', 'L15', 'L2', 'L20', 'L22', 'L26', 'L28', 'L3', 'L35', 'L36', 'L39', 'L4', 'L41', 'L46', 'L5', 'L6', 'L9'], 70: ['11A', '17', '221', '300', '306', '316', '328', '329', '330', '370', '396', '71', '79', 'CE1', 'DM1', 'L12', 'L31', 'L33', 'U11C', 'XA1'], 80: [] }
 var buses = null;
 var map = null;
 var groupMarkers = null;
 var markers = new Array();
 var bounds = null;
-
+var firstTime = true;  // to ckeck if it is the first time that searches or is it an update 
+let timeOut = null;
 
 
 
@@ -34,18 +35,19 @@ $(document).ready(function () {
         console.log("error: " + event);
     }
 
-    $("#pwa_btn").click(()=>{
-        if (beforeInstallPrompt) {beforeInstallPrompt.prompt()};
+    $("#pwa_btn").click(() => {
+        if (beforeInstallPrompt) { beforeInstallPrompt.prompt() };
     })
+
     //button to show modal if there isnt an event
-    setTimeout(()=>{
-        if(!beforeInstallPrompt){
+    setTimeout(() => {
+        if (!beforeInstallPrompt) {
             document.getElementById("pwa_btn").removeAttribute("disabled");
-            $("#pwa_btn").attr('data-bs-toggle',"modal")
-            $("#pwa_btn").attr("data-bs-target","#exampleModal")
+            $("#pwa_btn").attr('data-bs-toggle', "modal")
+            $("#pwa_btn").attr("data-bs-target", "#exampleModal")
 
         }
-    } ,500)
+    }, 1000)
 
 
 
@@ -62,7 +64,7 @@ $(document).ready(function () {
     function updateDB() {
         $.ajax(
             {
-                url: "https://donde-estan-buses.deno.dev/updateDB",
+                url: `https://${DENO_URL}/updateDB`,
                 method: "get",
                 contentType: "application/json",
 
@@ -77,48 +79,63 @@ $(document).ready(function () {
     updateDB();
 
     //ROUTES BUTTON 
-    $("#routes_btn").click(()=>{
+    $("#routes_btn").click(() => {
         alert("Nuevas funciones, inclyendo los recorridos aparecerán pronto!");
     }
     )
     //////
 
-
+    // SEARCH BUTTON
     $("#search").click(async () => {
+        if (firstTime) {
+            ajaxSearchBuses()
+            console.log("hola hola")
+        } else {
+            if (!timeOut) {
+                ajaxSearchBuses()
+                timeOut = setTimeout(() => { clearTimeout(timeOut); timeOut = null }, 2000)
+            }
+        }
+    }
+    )
+    const ajaxSearchBuses = () => {
         $.ajax(
             {
-                url: "https://donde-estan-buses.deno.dev/buses?codigoEmpresa=" + $("#empresa").val() + "&linea=" + $("#segundo").val(),
+                url: `https://${DENO_URL}/buses?codigoEmpresa=` + $("#empresa").val() + "&linea=" + $("#linea").val(),
                 method: "get",
                 contentType: "application/json",
                 // data:JSON.stringify({"subsistema":"-1","empresa":"-1"}),
-                success: (response) => { showResponseBuses(response) }
+                success: (response) => {
+                    showResponseBuses(response)
+                    console.log(response)
+                }
             }
         )
     }
-    )
     //array for the markers of the map
     // var destinos = [];
     function showResponseBuses(response) {
         //console.log(response)
         $("#search").html("Actualizar")
         buses = response.data;
-
+        buses.forEach(bus => { bus.geometry.coordinates.reverse() })
+        last = response.lastUpdateDB;
+        console.log(last)
         if (buses.length > 0) {
             destinos = new Array(); // to save later destinations to choose from
-            new Promise(function (resolve) {
-                showBuses(buses)
-                resolve()
-            }).then(function () {
-                map.fitBounds(bounds)
-            })
-
             buses.forEach(bus => {
                 if (destinos.indexOf(bus.properties.destinoDesc) === -1) { destinos.push(bus.properties.destinoDesc) };//make a list of the destinations so u can show them as button to select
             });
-            adddestinatiosHtml();
+            if (firstTime) {
+                adddestinatiosHtml();
+            }
+            new Promise(function (resolve) {
+                filterBusesByDest();
+                resolve()
+            })
+
 
             $("#alert_tiempo").empty();
-
             if (((Date.now() - response.lastUpdateDB) >= 20000)) {
                 $("#alert_tiempo").empty()
                     .append('<div class="alert alert-warning alert-dismissible fade show" role="alert">Las ubicaciones no están actualizadas, intente de nuevo en unos segundos.(diferencia ' + (Date.now() - response.lastUpdateDB) / 1000 + 'segundos)<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button> </div>')
@@ -137,6 +154,8 @@ $(document).ready(function () {
                 .append('<div class="alert alert-warning alert-dismissible fade show" role="alert">No hay ubicaciones reportadas para su búsqueda, intente nuevamente.<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button> </div>')
 
         }
+        firstTime = false
+
     }
 
 
@@ -168,10 +187,6 @@ $(document).ready(function () {
             destChecked.push(($(this).val()))
         })
         buesesChecked = buses.filter(bus => destChecked.includes(bus.properties.destinoDesc)) //
-
-
-        showBuses(buesesChecked);
-
         showBuses(buesesChecked);
 
     };
@@ -179,76 +194,47 @@ $(document).ready(function () {
 
     function showBuses(busess) { //add buses to the map recived as an array
         //remove markers if there are any'
-        //removeAllMarkers()
-        //removeAllMarkers();
         new Promise(function (resolve) {
             map.removeLayer(groupMarkers)
             groupMarkers = L.featureGroup();
             resolve();
-        }).then(function () {
+        }).then(() => {
             busess.forEach((bus) => {
-                const marker = L.marker(bus.geometry.coordinates.reverse())
+                const marker = L.marker(bus.geometry.coordinates) //removed reverse()
                 marker.bindPopup("<b>" + bus.properties.linea + "</b></br>Destino: " + bus.properties.destinoDesc + "</br> Tipo: " + bus.properties.sublinea)
                 // marker.addTo(map)
                 markers.push(marker)
-
                 marker.addTo(groupMarkers)
             })
             groupMarkers.addTo(map);
-            bounds = groupMarkers.getBounds();
+            new Promise((resolve) => {
+                bounds = null
+                bounds = groupMarkers.getBounds();
+                resolve()
+            }).then(() => {
+                if (busess.length > 0) {
+                    map.fitBounds(bounds)
+                }
+            }
+            )
         })
     }
 
-    //    const changeMapViewport = (buses)=>{
-    //     var maxLat = null
-    //     var maxLong = null
-    //     var minLat= null
-    //     var minLong= null
-    //     buses.forEach(bus =>{
-    //         if(bus.geometry.coordinates[1] > maxLat){ maxLat = bus.geometry.coordinates[1]}
-    //         if(bus.geometry.coordinates[1] < minLat){ minLat = bus.geometry.coordinates[1]}
-    //         if(bus.geometry.coordinates[0] > maxLong){ maxLong = bus.geometry.coordinates[0]}
-
-    //         if(bus.geometry.coordinates[0] < minLong){ minLong = bus.geometry.coordinates[0]}
-    //     })
-    //     map.fitBounds([
-    //         [minLat, minLong],
-    //         [maxLat, maxLong]
-    //     ]);
-    //    }
-
-
-
-
-    // function removelayer(marker){
-    //     console.log("Borrando markers del mapa:")
-    //     console.log(marker)
-    //     map.removeLayer(marker)
-    // }
-
-
-    // function addlayer(marker){
-    //     console.log("Añadir markers del mapa:")
-    //     console.log(marker)
-    //     map.addLayer(marker)
-    // }
-
-
 
     $("#empresa").change(function () {
+        firstTime = true
         var empresa = $("#empresa").val();
         // empresa = document.getElementById("empresa").value;
-        $(".segundo").removeClass("invisible");
-        $("#segundo").empty()
+        $(".segundo").removeAttr("disabled");
+        $("#linea").empty()
         lineas[empresa].forEach(i => {
-            $("#segundo").append('<option value=' + i + '>' + i + '</option>')
+            $("#linea").append('<option value=' + i + '>' + i + '</option>')
         });
     })
-    // if (empresa == "COPSA") {
-    //     lineas[empresa].forEach(i => {
-    //     $("#segundo").append('<option value='+i+'>'+i+'</option>')
-    //     });
-    // }
+    $("#linea").change(() => {
+        firstTime = true
+    }
+    )
 
     let empresas = [
         {
@@ -275,7 +261,7 @@ $(document).ready(function () {
             codigo: '18',
             descripcion: 'C.O.P.S.A.'
         },
-        
+
         {
             codigo: '50',
             descripcion: 'C.U.T.C.S.A.'
@@ -284,7 +270,7 @@ $(document).ready(function () {
             codigo: '32',
             descripcion: 'SATT'
         },
-       
+
         {
             codigo: '35',
             descripcion: 'TALA-PANDO-MONTEVIDEO'
@@ -297,8 +283,8 @@ $(document).ready(function () {
             codigo: '39',
             descripcion: 'ZEBALLOS HERMANOS'
         },
-        
-        
+
+
 
     ];
 
@@ -310,12 +296,3 @@ $(document).ready(function () {
 
 
 })
-
-
-// <select id="subsistema" name="subsistema" class="form-control">
-// 										<option value="-1">Todos</option>
-// 										<option value="1">Montevideo</option>
-// 										<option value="2">Canelones</option>
-// 										<option value="3">San Jose</option>
-// 										<option value="4">Metropolitano</option>
-// 									</select>
