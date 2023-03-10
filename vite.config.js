f4a78578d80422b98400ca95183b6256bba0229f
matiasr1608 <69040294+matiasr1608@users.noreply.github.com>
2023-03-10 19:20:20 -0300

    added vite boundler

---
+++ vite.config.js
@@ -0,0 +1,72 @@
+import inject from '@rollup/plugin-inject';
+import { VitePWA } from 'vite-plugin-pwa'
+
+
+const path = require('path')
+
+export default {
+  root: path.resolve(__dirname, 'src'),
+  resolve: {
+    alias: {
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+    }
+  },
+  build: {
+    emptyOutDir: true,
+    outDir: '../dist',
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'src/index.html'),
+        about: path.resolve(__dirname, 'src/about.html'),
+        routes: path.resolve(__dirname, 'src/routes.html')
+      }
+    },
+  },
+  publicDir: '../public',
+  server: {
+    port: 8080,
+    hot: true
+  },
+  plugins: [
+    // Add it first
+    inject({
+      $: 'jquery',
+    }),
+    //add the pwa and manifest
+    VitePWA({
+      injectRegister: 'inline',
+      registerType: 'autoUpdate',
+      //includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      includeAssets: [
+        '/*.png'
+      ],
+      devOptions: {
+        enabled: true
+        /* other options */
+      },
+      manifest: {
+        name: "¿Dónde está Mi Bondi?",
+        short_name: "MiBondi",
+        start_url: "index.html",
+        display: "standalone",
+        background_color: "#fdfdfd",
+        theme_color: "#0462DC",
+        orientation: "portrait-primary",
+        icons: [ {
+          src: "logo_512.png",
+          type: "image/png", 
+          sizes: "512x512", 
+          purpose: "any maskable"
+        },
+        {
+          src: "logo_225_blue.png",
+          type: "image/png", 
+          sizes: "225x225"
+        }
+        ]
+
+      }
+    })
+  ],
+  // The rest of your configuration...
+};
